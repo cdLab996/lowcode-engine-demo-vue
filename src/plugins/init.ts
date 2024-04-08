65ff7323abb4d033b675627e85b8d09f17291dc2
wudi <wuchendi96@gmail.com>
2024-04-08 18:11:08 +0800

    feat: Using the @cdlab996 package material component library.

--- src/plugins/init.ts
+++ src/plugins/init.ts
@@ -1,9 +1,7 @@
 import { injectAssets } from '@alilc/lowcode-plugin-inject';
 import { getProjectSchemaToLocalStorage } from '@/utils/store';
-import { IPublicModelPluginContext, IPublicTypeAssetsJson } from '@alilc/lowcode-types';
+import { IPublicModelPluginContext } from '@alilc/lowcode-types';
 import assets from '@/assets/assets.json';
-import vant from '@/assets/vant.json';
-import elementPlus from '@/assets/element-plus.json';
 import originSchema from '@/assets/schema.json';
 
 const editorInit = (ctx: IPublicModelPluginContext) => {
@@ -11,11 +9,15 @@ const editorInit = (ctx: IPublicModelPluginContext) => {
     name: 'editor-init',
     async init() {
       const { material, project } = ctx;
+
+      // const assets = await fetch('http://127.0.0.1:9000/assets.json').then((res) =>
+      //   res.json()
+      // );
+      // console.log('🚀 ~ file: init.ts:16 ~ init ~ assets:', assets);
+      // material.setAssets(assets);
+
       const loadedAssets = await injectAssets(assets);
       material.setAssets(loadedAssets);
-
-      material.loadIncrementalAssets(elementPlus as IPublicTypeAssetsJson);
-      material.loadIncrementalAssets(vant as IPublicTypeAssetsJson);
 
       const projectSchema = getProjectSchemaToLocalStorage();
       const schema = projectSchema ? projectSchema['componentsTree'].pop() : originSchema;
